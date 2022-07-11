be7c4811fa56ffbd377584c0b546a94c9cd585a0
WaiswaDonnie <waiswadonnie@gmail.com>
2022-07-11 19:11:06 +0300

    added pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
 import Products from './components/Products';
-
+import { Routes, Route, Link } from "react-router-dom";
+import Login from './pages/loginpage';
+import Dashboard from './pages/dashboard';
 function App() {
   return (
-    <div>
-      <Products/>
-    </div>
+    // <div>
+    //   <Products/>
+    // </div>
+    <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './components/Navbar';
-
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar/>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
